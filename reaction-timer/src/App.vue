c464112a7ea6100f42cb14c75ac25ef75c24869c
Ye99-AungKhant <yeaungkhant077@gmail.com>
2022-11-25 15:07:40 +0630

    reaction timer

---
+++ reaction-timer/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Start</button>
+  <Block v-if="isPlaying" :delaytime="delay" @end="endGame"/>
+
+  <Results v-if="showResult" :score="score"/>
+  <!-- <p v-if="showResult">Reaction Time : {{score}} ms</p> -->
+</template>
+
+<script>
+
+import Block from './components/Block'
+import Results from './components/Results'
+
+export default {
+  name: 'App',
+  components: {
+   Block, Results
+  },
+  data(){
+    return{
+      isPlaying: false,
+      delay: null,
+      score:null,
+      showResult:false
+    }
+  },
+  methods:{
+    start(){
+      this.isPlaying = true,
+      this.delay = 2000 + Math.random() * 5000
+      this.showResult = false
+      console.log(this.delay)
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.isPlaying =false
+      this.showResult =true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
---
+++ reaction-timer/src/components/Block.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="block" v-if="showBlock" @click="stopTimer">
+        click me
+    </div>
+</template>
+
+<script>
+export default{
+    props:['delaytime'],
+    data(){
+        return{
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
+        }
+    },
+    mounted(){
+        console.log('component mounted');
+        setTimeout(() => {
+            this.showBlock = true
+            this.startTimer()
+        }, this.delaytime);
+    },
+    methods:{
+        startTimer(){
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10)
+        },
+        stopTimer(){
+            clearInterval(this.timer)
+            this.$emit('end',this.reactionTime)
+            console.log(this.reactionTime)
+        }
+    }
+}
+</script>
+
+<style>
+    .block{
+        width: 400px;
+        border-radius: 20px;
+        background: #0faf87;
+        color: white;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+</style>
